3ac6117cbf7672865cbb8dabd1929377a81cf1aa
brayansantosunam-png <brayan.santos.unam@gmail.com>
2026-01-21 19:50:13 -0600

    Add files via upload

--- logica.js
+++ logica.js
@@ -1,14 +1,15 @@
 const songs = [
     { title: "Soñé", artist: "Zoé", phrase: "Pues no tengo nada que perder...", cover: "https://cdn-images.dzcdn.net/images/cover/8498486810fb5956153f175822b7b7d8/0x1900-000000-80-0-0.jpg", src: "sone.m4a" },
-    { title: "Droopy likes your Face", artist: "Minecraft Volume Alpha", phrase: "Entre cubos y nostalgia", cover: "https://cdn-images.dzcdn.net/images/cover/a6e61f6327038b8dd63ec08509c69244/0x1900-000000-80-0-0.jpg", src: "minecraft_uno.m4a" },
-    { title: "Ser parte", artist: "Siddhartha", phrase: "Te admiraré, ser parte, quiza...", cover: "https://cdn-images.dzcdn.net/images/cover/a508833ee74e2cd3197f0641e3c73545/1900x1900-000000-80-0-0.jpg", src: "ser_parte.mp3" },
+    { title: "Droopy likes your Face", artist: "Minecraft Volume Alpha", phrase: "Entre cubos y nostalgia...", cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiYA9JFGnmzLKIhmwtyy3JimNGHTFg8cXoknIPVR_DM2J-LRxvfX4MhklD3xLGkb7ZOyDyyhsuWXLA6zOSLr2uQibClGQoETpfKGBfiYn5frqtOtFHjc4sMRcS2Uhr62OGSplpKt7qbjp3hyphenhyphenf84nSFgQ4YToMRs9a0eFGaZQ53hB0pXQ5av5CK2bWtPC9M/s640/minecraft.jpg", src: "minecraft_uno.m4a" },
     { title: "Labios rotos", artist: "Zoé", phrase: "En el desierto de mi alma...", cover: "https://cdn-images.dzcdn.net/images/cover/8498486810fb5956153f175822b7b7d8/0x1900-000000-80-0-0.jpg", src: "labiosrotos.m4a" },
     { title: "Tarde", artist: "Siddhartha", phrase: "Tarde se me hacía para volver a verte...", cover: "https://cdn-images.dzcdn.net/images/cover/a508833ee74e2cd3197f0641e3c73545/1900x1900-000000-80-0-0.jpg", src: "tarde.m4a" },
-    { title: "Ojos noche", artist: "Elsa y el mar", phrase: "Con tus ojos de noche (Eh-eh, eh-eh)...", cover: "https://i.scdn.co/image/ab67616d0000b27315b34a0c485e552542339421", src: "ojosnoche.m4a" },
-    { title: "Tears", artist: "Minecraft", phrase: "Geometría del espacio...", cover: "https://cdn-images.dzcdn.net/images/cover/a6e61f6327038b8dd63ec08509c69244/0x1900-000000-80-0-0.jpg", src: "minecraft_dos.m4a" },
+    { title: "Me hace falta", artist: "Siddhartha", phrase: "Con la luz intermitente, Vibra tu frecuencia...", cover: "https://images.genius.com/4d612579eb7472076b70e67617e12fb6.1000x1000x1.png", src: "me_hace_falta.mp3" },
+    { title: "Tears", artist: "Minecraft", phrase: "Fantastica geometría multicolor...", cover: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiYA9JFGnmzLKIhmwtyy3JimNGHTFg8cXoknIPVR_DM2J-LRxvfX4MhklD3xLGkb7ZOyDyyhsuWXLA6zOSLr2uQibClGQoETpfKGBfiYn5frqtOtFHjc4sMRcS2Uhr62OGSplpKt7qbjp3hyphenhyphenf84nSFgQ4YToMRs9a0eFGaZQ53hB0pXQ5av5CK2bWtPC9M/s640/minecraft.jpg", src: "minecraft_dos.m4a" },
     { title: "Vía lactea", artist: "Zoé", phrase: "Del prisma de tus ojos, en mi caso de astronauta...", cover: "https://cdn-images.dzcdn.net/images/cover/8498486810fb5956153f175822b7b7d8/0x1900-000000-80-0-0.jpg", src: "via_lactea.mp3" },
     { title: "Bruma", artist: "León Larregui", phrase: "Dar sin esperar nada a cambio...", cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAKhj6DATZm2qKUx11gFHedys-WEO3UfFgUQ&s", src: "bruma.m4a" },
-    { title: "Velur", artist: "Zoé", phrase: "Y me ajusto, pero no me aplaco... Ni rosas, ni avellanas echa formol al corazón.", cover: "https://cdn-images.dzcdn.net/images/cover/46d64f553900fcee92fdc8e364246828/0x1900-000000-80-0-0.jpg", src: "velur.m4a" }
+    { title: "Velur", artist: "Zoé", phrase: "Me ajusto, pero no me aplaco... Ni rosas, ni avellanas echa formol al corazón...", cover: "https://cdn-images.dzcdn.net/images/cover/46d64f553900fcee92fdc8e364246828/0x1900-000000-80-0-0.jpg", src: "velur.m4a" },
+    { title: "Ser parte", artist: "Siddhartha", phrase: "Reinventarme y ser parte, al final si todo va a cambiar me guiare por tí...", cover: "https://cdn-images.dzcdn.net/images/cover/a508833ee74e2cd3197f0641e3c73545/1900x1900-000000-80-0-0.jpg", src: "ser_parte.mp3" }
+    
 ];
 
 let typewriterInterval; // Variable global para controlar la animación
@@ -102,6 +103,10 @@ function prevSong() {
     selectSong(currentSongIndex);
 }
 
+audio.addEventListener('ended', () => {
+    nextSong(); 
+});
+
 // Esperamos a que el HTML esté listo
 document.addEventListener('DOMContentLoaded', () => {
     
@@ -123,7 +128,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // 3. Cargar la playlist inicial
     loadPlaylist();
+
+    // INICIO AUTOMÁTICO:
+    // Cargamos y reproducimos la primera canción inmediatamente
+    selectSong(0); 
+    
+    // Opcional: Intentar reproducir (el navegador podría bloquearlo hasta el primer clic)
+    audio.play().catch(error => {
+        console.log("El autoplay fue bloqueado por el navegador. Esperando interacción del usuario.");
+    });
+
 });
+
 
 /* ------------------------------------------------------ Seek bar --------------------------------------------- */
 
@@ -172,4 +188,42 @@ function formatTime(seconds) {
     const min = Math.floor(seconds / 60);
     const sec = Math.floor(seconds % 60);
     return `${min}:${sec < 10 ? '0' + sec : sec}`;
+}
+
+//---------------------------------------------------- Footer ------------------------------------------------
+document.addEventListener('DOMContentLoaded', () => {
+    const footerText = document.querySelector('.footer-copy .lang');
+
+    if (footerText) {
+        // Guardamos el texto original para poder volver a él después
+        const textoOriginal = footerText.innerText;
+        // Obtenemos la frase alternativa del atributo data-en
+        const textoAlternativo = footerText.getAttribute('data-en');
+
+        // Evento: Cuando el mouse entra
+        footerText.addEventListener('mouseenter', () => {
+            footerText.innerText = textoAlternativo;
+        });
+
+        // Evento: Cuando el mouse sale
+        footerText.addEventListener('mouseleave', () => {
+            footerText.innerText = textoOriginal;
+        });
+    }
+});
+
+//---------------------------------------------------- Switch ------------------------------------------------
+
+const themeSwitch = document.getElementById('themeSwitch');
+
+if (themeSwitch) {
+    themeSwitch.addEventListener('change', function() {
+        if (this.checked) {
+            // Si el switch está activo, añadimos la clase
+            document.body.classList.add('alt-theme');
+        } else {
+            // Si se desactiva, volvemos al fondo original
+            document.body.classList.remove('alt-theme');
+        }
+    });
 }
